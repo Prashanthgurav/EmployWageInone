1d7e6d3f29ef6d5780cd2da2f214694ca4094fb6
prashantgurav <guravp@gmail.com>
2023-04-30 12:41:53 +0530

    [Prashnt] Add employ wage programs

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Metadata.W3cXsd2001;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployWageBasic
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+     
+
+            Console.WriteLine("Welcome To The EmployWage Basic Core Program");
+            Console.WriteLine("Please Choose One From Below Option: ");
+            Console.WriteLine(" 1 ");
+            int Option = Convert.ToInt32(Console.ReadLine());
+           
+        }
+    }
+}
